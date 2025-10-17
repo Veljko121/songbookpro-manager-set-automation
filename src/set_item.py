aeacfcb80ae1b5ec9015b7070593938a0783dcc4
Veljko Nikolić <nikolicveljko01@gmail.com>
2025-10-17 18:06:43 +0200

    Deleted unnecessary order field in SetItem

--- src/set_item.py
+++ src/set_item.py
@@ -1,9 +1,9 @@
 from song import Song
 
 class SetItem:
-    def __init__(self, song: Song, order: int, set_key: int):
+    
+    def __init__(self, song: Song, set_key: int):
         self.song = song
-        self.order = order
         self.set_key = set_key
 
     def key_offset(self):
