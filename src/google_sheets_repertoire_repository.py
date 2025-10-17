bc7d55c9c28762d4d41eff1f898bf7666b86f840
Veljko Nikolić <nikolicveljko01@gmail.com>
2025-10-17 18:06:57 +0200

    Added song key mapping

--- src/google_sheets_repertoire_repository.py
+++ src/google_sheets_repertoire_repository.py
@@ -1,5 +1,33 @@
 import gspread
-from set_item import SetItem
+
+keys = {
+    "A"  :  0,
+    "B"  :  1,
+    "H"  :  2,
+    "C"  :  3,
+    "Db" :  4, "C#" :  4,
+    "D"  :  5,
+    "Eb" :  6, "D#" :  6,
+    "E"  :  7,
+    "F"  :  8,
+    "F#" :  9, "Gb" :  9,
+    "G"  : 10,
+    "Ab" : 11, "G#" : 11, 
+    "F#m": 12, "Gbm": 12,
+    "Gm" : 13,
+    "G#m": 14, "Abm": 14,
+    "Am" : 15,
+    "Bm" : 16,
+    "Hm" : 17,
+    "Cm" : 18,
+    "C#m": 19, "Dbm": 19,
+    "Dm" : 20,
+    "D#m": 21, "Ebm": 21,
+    "Em" : 22,
+    "Fm" : 23,
+
+    "None" : -1,
+}
 
 def format_song_name(name: str):
     return name.strip().replace("‘", "'").replace("’", "'")
@@ -13,7 +41,7 @@ class GoogleSheetsRepertoireRepository:
 
     def get_songs(self, songs_column: int = 1):
         return [
-            (format_song_name(song_name), key) 
+            (format_song_name(song_name), keys[key]) 
             for song_name, key
             in zip(self.sheet.col_values(songs_column), self.sheet.col_values(songs_column + 1))
         ]
