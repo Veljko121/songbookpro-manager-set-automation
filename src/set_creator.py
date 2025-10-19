0b3ff322cff5d5bad89af0a7627d0b1474678af4
Veljko Nikolić <nikolicveljko01@gmail.com>
2025-10-19 18:41:53 +0200

    Connected actions

--- src/set_creator.py
+++ src/set_creator.py
@@ -1,6 +1,7 @@
 from ui.ui_set_creator import Ui_SetCreator
-from PyQt6.QtWidgets import QWidget, QApplication
+from PyQt6.QtWidgets import QWidget, QApplication, QFileDialog
 from PyQt6.QtGui import QKeySequence, QShortcut
+from service import Service
 
 class SetCreator(QWidget):
     
@@ -13,7 +14,34 @@ class SetCreator(QWidget):
         shortcut = QShortcut(QKeySequence("Ctrl+Q"), self)
         shortcut.activated.connect(self.close)
 
+        # Service initialization
+        self.service = Service()
+
+        # UI singals setup
+        self._connect_actions()
+
         self.adjustSize()
+
+    def _connect_actions(self):
+        self.ui.browseCredentialsPushButton.clicked.connect(self.browse_credentials)
+        self.ui.spreadsheetsComboBox.currentIndexChanged.connect(self.update_sheets)
+
+    def browse_credentials(self):
+        file_path, _ = QFileDialog.getOpenFileName(self, "Open Spreadsheet", "", "Credentials JSON (*.json)")
+        if file_path:
+            self.ui.credentialsPathLineEdit.setText(file_path)
+            spreadsheets = self.service.get_available_google_spreadsheets(self.ui.credentialsPathLineEdit.text())
+            self.ui.spreadsheetsComboBox.clear()
+            for spreadsheet in spreadsheets:
+                self.ui.spreadsheetsComboBox.addItem(spreadsheet.title, spreadsheet.id)
+    
+    def update_sheets(self):
+        self.ui.sheetsComboBox.clear()
+        sheets_selection = self.ui.repertoireTabWidget.currentIndex()
+        if sheets_selection == 0:
+            sheets = self.service.get_sheets(self.ui.repertoireTabWidget.currentIndex(), { "spreadsheet_id": self.ui.spreadsheetsComboBox.currentData() })
+            for sheet in sheets:
+                self.ui.sheetsComboBox.addItem(sheet.title, sheet.id)
 
 if __name__ == "__main__":
     import sys
