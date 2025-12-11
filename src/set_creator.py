353a99c7fd2927f3f1320512fa351448911a6805
Veljko Nikolić <nikolicveljko01@gmail.com>
2025-12-11 16:19:34 +0100

    Simplified the UI
    Sheets combobox for each tab

--- src/set_creator.py
+++ src/set_creator.py
@@ -3,6 +3,8 @@ from PyQt6.QtWidgets import QWidget, QApplication, QFileDialog
 from PyQt6.QtGui import QKeySequence, QShortcut
 from service import Service
 
+# 
+
 class SetCreator(QWidget):
     
     def __init__(self):
@@ -24,39 +26,22 @@ class SetCreator(QWidget):
 
     def _connect_actions(self):
         self.ui.browseCredentialsPushButton.clicked.connect(self.browse_credentials)
-        self.ui.spreadsheetsComboBox.currentIndexChanged.connect(self.update_sheets)
-        self.ui.repertoireTabWidget.currentChanged.connect(self.repertoire_tab_changed)
-
-    def repertoire_tab_changed(self):
-        sheets_selection = self.ui.repertoireTabWidget.currentIndex()
-        if sheets_selection == 0:
-            self.load_google_repertoires()
-
-    def load_google_repertoires(self):
-        credentials_path = self.ui.credentialsPathLineEdit.text()
-        if credentials_path:
-            spreadsheets = self.spreadsheets
-            if not spreadsheets:
-                spreadsheets = self.service.get_available_google_spreadsheets(credentials_path)
+        self.ui.googleSpreadsheetsComboBox.currentIndexChanged.connect(self.update_google_sheets)
 
     def browse_credentials(self):
         file_path, _ = QFileDialog.getOpenFileName(self, "Open credentials", "", "Credentials JSON (*.json)")
         if file_path:
             self.ui.credentialsPathLineEdit.setText(file_path)
             spreadsheets = self.service.get_available_google_spreadsheets(self.ui.credentialsPathLineEdit.text())
-            self.ui.spreadsheetsComboBox.clear()
+            self.ui.googleSpreadsheetsComboBox.clear()
             for spreadsheet in spreadsheets:
-                self.ui.spreadsheetsComboBox.addItem(spreadsheet.title, spreadsheet.id)
+                self.ui.googleSpreadsheetsComboBox.addItem(spreadsheet.title, spreadsheet.id)
     
-    def update_sheets(self):
-        self.ui.sheetsComboBox.clear()
-        sheets_selection = self.ui.repertoireTabWidget.currentIndex()
-        if sheets_selection == 0:
-            sheets = self.service.get_sheets(self.ui.repertoireTabWidget.currentIndex(), { "spreadsheet_id": self.ui.spreadsheetsComboBox.currentData() })
-            for sheet in sheets:
-                self.ui.sheetsComboBox.addItem(sheet.title, sheet.id)
-        elif sheets_selection == 1:
-            pass # TODO: local sheets
+    def update_google_sheets(self):
+        self.ui.googleSheetsComboBox.clear()
+        sheets = self.service.get_sheets(self.ui.repertoireTabWidget.currentIndex(), { "spreadsheet_id": self.ui.googleSpreadsheetsComboBox.currentData() })
+        for sheet in sheets:
+            self.ui.googleSheetsComboBox.addItem(sheet.title, sheet.id)
 
 if __name__ == "__main__":
     import sys
