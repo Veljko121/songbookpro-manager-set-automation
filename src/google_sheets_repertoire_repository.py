90908780ceabda4b1f0f2d3d4500d7341e1aa033
Veljko Nikolić <nikolicveljko01@gmail.com>
2026-04-03 16:53:54 +0200

    Removed unnecessary methods

--- src/google_sheets_repertoire_repository.py
+++ src/google_sheets_repertoire_repository.py
@@ -2,41 +2,10 @@ import gspread
 from base_repertoire_repository import BaseRepertoireRepository
 from typing import List, Tuple
 
-keys = {
-    "A"  :  0,
-    "B"  :  1,
-    "H"  :  2,
-    "C"  :  3,
-    "Db" :  4, "C#" :  4,
-    "D"  :  5,
-    "Eb" :  6, "D#" :  6,
-    "E"  :  7,
-    "F"  :  8,
-    "F#" :  9, "Gb" :  9,
-    "G"  : 10,
-    "Ab" : 11, "G#" : 11, 
-    "F#m": 12, "Gbm": 12,
-    "Gm" : 13,
-    "G#m": 14, "Abm": 14,
-    "Am" : 15,
-    "Bm" : 16,
-    "Hm" : 17,
-    "Cm" : 18,
-    "C#m": 19, "Dbm": 19,
-    "Dm" : 20,
-    "D#m": 21, "Ebm": 21,
-    "Em" : 22,
-    "Fm" : 23,
-
-    "None" : -1,
-}
-
-def format_song_name(name: str):
-    return name.strip().replace("‘", "'").replace("’", "'")
-
 class GoogleSheetsRepertoireRepository(BaseRepertoireRepository):
 
     def __init__(self, google_sheets_client: gspread.client.Client, google_spreadsheet_id: str, google_sheet: str):
+        super().__init__()
         self.client = google_sheets_client
         self.google_spreadsheet_id = google_spreadsheet_id
         self.google_sheet = google_sheet
