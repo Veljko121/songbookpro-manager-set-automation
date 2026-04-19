59f4416e89743e205b4ec95dc4af7291d208ce9f
Veljko Nikolić <nikolicveljko01@gmail.com>
2026-04-19 22:35:15 +0200

    Added types

--- src/set.py
+++ src/set.py
@@ -1,5 +1,8 @@
+from typing import List
+from set_item import SetItem
+
 class Set:
 
-    def __init__(self, name, items: list):
+    def __init__(self, name, items: List[SetItem]):
         self.name = name
         self.items = items
